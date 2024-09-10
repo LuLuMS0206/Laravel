42a98d85c16a8db07f977e13fb8204e4b15c593d
LuLuMS0206 <luciamacho00@gmail.com>
2024-09-10 10:17:38 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ActivityController;
+
+Route::resource('activities', ActivityController::class);
 
 Route::get('/', function () {
     return view('welcome');
